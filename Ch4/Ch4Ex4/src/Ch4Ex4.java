e667531f38f644e1480c6bccf9da24cc858410ef
Brent Perry <burentoperii@gmail.com>
2016-08-09 14:25:00 -0500

    Change Ch4Ex4 to be exclusive OR.

--- Ch4/Ch4Ex4/src/Ch4Ex4.java
+++ Ch4/Ch4Ex4/src/Ch4Ex4.java
@@ -2,7 +2,7 @@
  * File: Ch4Ex4.java
  * -----------------
  * This program prints all numbers between 1 and 100 that are
- * divisible by 6 or 7.
+ * divisible by either 6 or 7, but not both.
  */
 
 import acm.program.*;
@@ -10,9 +10,9 @@ import acm.program.*;
 public class Ch4Ex4 extends ConsoleProgram {
 	public void run() {
 		println("This program prints all numbers between 1 and 100,");
-		println("that are divisible by 6 or 7.");
+		println("that are divisible by either 6 or 7, but not both.");
 		for (int i = 1; i <= 100; i++) {
-			if (i % 6 == 0 || i % 7 == 0) println(i);
+			if ((i % 6 == 0 && i % 7 != 0) || (i % 6 != 0 && i % 7 == 0)) println(i);
 		}
 	}
 }
