51d395a44efa0b10c34269c537ff5028c6842e97
Brent Perry <burentoperii@gmail.com>
2016-08-10 17:11:03 -0500

    Add functionality to Ch4Ex13 to show second largest as well.

--- Ch4/Ch4Ex13/src/Ch4Ex13.java
+++ Ch4/Ch4Ex13/src/Ch4Ex13.java
@@ -2,7 +2,8 @@
  * File: Ch4Ex13.java
  * ------------------
  * This program reads in a series of integers from the user, until the 
- * sentinel is entered. Then it displays the largest number entered.
+ * sentinel is entered. Then it displays the largest number and the 
+ * second largest entered.
  */
 
 import acm.program.*;
@@ -13,17 +14,23 @@ public class Ch4Ex13 extends ConsoleProgram {
 	
 	public void run() {
 		println("This program prompts you for a series of numbers, and");
-		println("then displays the largest one entered.");
+		println("then displays the largest, and second largest ones entered.");
 		println("Enter '0' to terminate the program.");
 		
 		int max = 0;
+		int second_max = 0;
 		
 		while (true) {
 			int input = readInt(" ? ");
 			if (input == SENTINEL) break;
-			if (input > max) max = input;
+			if (input > max) {
+				max = input;
+			} else if (input > second_max) {
+				second_max = input;
+			}
 		}
 		
 		println("The largest value is " + max);
+		println("The second largest is " + second_max);
 	}
 }
