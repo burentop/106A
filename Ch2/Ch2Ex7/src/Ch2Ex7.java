2308cb5d566ace7b4a88d537bb820e92dcc2364d
Brent Perry <burentoperii@gmail.com>
2016-08-10 15:25:33 -0500

    Make rings thicker in Ch2Ex7

--- Ch2/Ch2Ex7/src/Ch2Ex7.java
+++ Ch2/Ch2Ex7/src/Ch2Ex7.java
@@ -24,10 +24,42 @@ public class Ch2Ex7 extends GraphicsProgram {
 		GOval red = new GOval(40, 40);
 		red.setColor(Color.RED);
 		
+		GOval blue2 = new GOval(40, 40);
+		blue2.setColor(Color.BLUE);
+		GOval yellow2= new GOval(40, 40);
+		yellow2.setColor(Color.YELLOW);
+		GOval black2 = new GOval(40, 40);
+		black2.setColor(Color.BLACK);
+		GOval green2 = new GOval(40, 40);
+		green2.setColor(Color.GREEN);
+		GOval red2 = new GOval(40, 40);
+		red2.setColor(Color.RED);
+		
+		GOval blue3 = new GOval(40, 40);
+		blue3.setColor(Color.BLUE);
+		GOval yellow3 = new GOval(40, 40);
+		yellow3.setColor(Color.YELLOW);
+		GOval black3 = new GOval(40, 40);
+		black3.setColor(Color.BLACK);
+		GOval green3 = new GOval(40, 40);
+		green3.setColor(Color.GREEN);
+		GOval red3 = new GOval(40, 40);
+		red3.setColor(Color.RED);
+		
 		add(blue, 5, 5);
+		add(blue2, 6, 6);
+		add(blue3, 7, 7);
 		add(black, 55, 5);
+		add(black2, 56, 6);
+		add(black3, 57, 7);
 		add(red, 105, 5);
+		add(red2, 106, 6);
+		add(red3, 107, 7);
 		add(yellow, 30, 27);
+		add(yellow2, 31, 28);
+		add(yellow3, 32, 29);
 		add(green, 80, 27);
+		add(green2, 81, 28);
+		add(green3, 82, 29);
 	}
 }
