742e441e2932cb28d810e0c60f418afbd9058780
Brent Perry <burentoperii@gmail.com>
2016-08-11 10:41:23 -0500

    Add checkers to Ch4Ex14

--- Ch4/Ch4Ex14/src/Ch4Ex14.java
+++ Ch4/Ch4Ex14/src/Ch4Ex14.java
@@ -4,6 +4,8 @@
  * This program draws a checkerboard, complete with checkers.
  */
 
+import java.awt.Color;
+
 import acm.graphics.*;
 import acm.program.*;
 
@@ -16,7 +18,23 @@ public class Ch4Ex14 extends GraphicsProgram {
 				double y = i * sqSize;
 				GRect sq = new GRect(x, y, sqSize, sqSize);
 				sq.setFilled((i + j) % 2 != 0);
+				sq.setFillColor(Color.GRAY);
 				add(sq);
+				
+				/* Create red/black checkers */
+				GOval red = new GOval(sqSize * .8, sqSize * .8);
+				red.setFilled(true);
+				red.setFillColor(Color.RED);
+				
+				GOval black = new GOval(sqSize * .8, sqSize * .8);
+				black.setFilled(true);
+				black.setFillColor(Color.BLACK);
+				
+				if (((i + j) % 2 != 0) && (i < 3)) {
+					add(red, x + (sqSize * .1), y + (sqSize * .1));
+				} else if (((i + j) % 2 != 0) && (i > 4)) {
+					add(black, x + (sqSize * .1), y + (sqSize * .1));
+				}
 			}
 		}
 	}
