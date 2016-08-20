03f7dcb41e8bf01b0b2bdc4ade90e89b6c6745a3
Brent Perry <burentoperii@gmail.com>
2016-08-20 14:56:16 -0500

    Add check for sentence palindromes to Ch8Ex6

--- Ch8/Ch8Ex6/src/Ch8Ex6.java
+++ Ch8/Ch8Ex6/src/Ch8Ex6.java
@@ -7,12 +7,26 @@
  */
 
 import acm.program.*;
+import java.util.*;
 
 public class Ch8Ex6 extends ConsoleProgram {
 	public void run() {
 		println("'noon' is a palindrome: " + isPalindrome("noon"));
 		println("'breakfast' is a palindrome: " + isPalindrome("breakfast"));
 		println("'racecar' is a palindrome: " + isPalindrome("racecar"));
+		
+		println("Now we will check for sentence palindromes.");
+		println("Indicate the end of the input with a blank line.");
+		
+		while (true) {
+			String input = readLine("Enter a sentence: ");
+			if (input.equals("")) break;
+			if (isSentencePalindrome(input)) {
+				println("That sentence is a palindrome.");
+			} else {
+				println("That sentence is not a palindrome.");
+			}
+		}
 	}
 	
 	private boolean isPalindrome(String str) {
@@ -21,5 +35,14 @@ public class Ch8Ex6 extends ConsoleProgram {
 			if (str.charAt(i) != str.charAt(end - i)) return false;
 		}
 		return true;
+	}
+	
+	private boolean isSentencePalindrome(String str) {
+		StringTokenizer tokenizer = new StringTokenizer(str, " ,.';!:");
+		String withoutPunc = "";
+		while(tokenizer.hasMoreTokens()) {
+			withoutPunc += tokenizer.nextToken();
+		}
+		return isPalindrome(withoutPunc.toLowerCase());
 	}
 }
