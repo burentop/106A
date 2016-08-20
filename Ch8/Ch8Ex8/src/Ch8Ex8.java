245f203924849714042fcc48ce1ea87b6dc02753
Brent Perry <burentoperii@gmail.com>
2016-08-20 15:21:52 -0500

    Add Ch8Ex8 exercise

---
+++ Ch8/Ch8Ex8/src/Ch8Ex8.java
@@ -0,0 +1,48 @@
+/**
+ * File: Ch8Ex8.java
+ * -----------------
+ * This program returns the plural form of words.
+ * @author Brent Perry
+ *
+ */
+
+import acm.program.*;
+
+public class Ch8Ex8 extends ConsoleProgram {
+	public void run() {
+		println("The plural of 'class' is: " + createRegularPlural("class"));
+		println("The plural of 'beach' is: " + createRegularPlural("beach"));
+		println("The plural of 'cherry' is: " + createRegularPlural("cherry"));
+		println("The plural of 'noun' is: " + createRegularPlural("noun"));
+	}
+	
+	private String createRegularPlural(String word) {
+		switch(word.charAt(word.length() - 1)) {
+		case 's': case 'x': case 'z':
+			return word + "es";
+		case 'h':
+			if ((word.charAt(word.length() - 2) == 'c') || (word.charAt(word.length() - 2) == 's')) {
+				return word + "es";
+			} else {
+				return word + "s";
+			}
+		case 'y':
+			if (!isVowel(word.charAt(word.length() - 2))) {
+				return word.substring(0, word.length() - 1) + "ies";
+			} else {
+				return word + "s";
+			}
+		default:
+			return word + "s";
+		}
+	}
+	
+	private boolean isVowel(char ch) {
+		switch(ch) {
+		case 'a': case 'e': case 'i': case 'o': case 'u':
+			return true;
+		default:
+			return false;
+		}
+	}
+}
