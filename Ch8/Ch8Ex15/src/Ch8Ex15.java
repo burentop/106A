be9a2f5a965b7e329c61c68534fb4986ddb46f6b
Brent Perry <burentoperii@gmail.com>
2016-08-21 15:22:48 -0500

    Add acronym method

--- Ch8/Ch8Ex15/src/Ch8Ex15.java
+++ Ch8/Ch8Ex15/src/Ch8Ex15.java
@@ -14,12 +14,12 @@ public class Ch8Ex15 extends ConsoleProgram {
 		while (true) {
 			String input = readLine("Enter a line: ");
 			if (input.length() == 0) break;
-			println("The longest word is " + longestWord(input));
+			println("The acronym is " + acronym(input));
 		}
 	}
 	
 	private String longestWord(String line) {
-		StringTokenizer tokenizer = new StringTokenizer(line, " .,!;:");
+		StringTokenizer tokenizer = new StringTokenizer(line, DELIMITERS);
 		String longest = "";
 		while (tokenizer.hasMoreTokens()) {
 			String current = tokenizer.nextToken();
@@ -29,4 +29,15 @@ public class Ch8Ex15 extends ConsoleProgram {
 		}
 		return longest;
 	}
+	
+	private String acronym(String str) {
+		String result = "";
+		StringTokenizer tokenizer = new StringTokenizer(str, DELIMITERS);
+		while (tokenizer.hasMoreTokens()) {
+			result += Character.toUpperCase(tokenizer.nextToken().charAt(0));
+		}
+		return result;
+	}
+	
+	private static final String DELIMITERS = " ,.-;:!";
 }
