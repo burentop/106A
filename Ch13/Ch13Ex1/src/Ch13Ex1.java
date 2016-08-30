62e96fdef8612307801ff410982d555d30fdfe2b
Brent Perry <burentoperii@gmail.com>
2016-08-30 10:46:20 -0500

    Add Ch13Ex1 exercise

--- Ch13/Ch13Ex1/src/Ch13Ex1.java
+++ Ch13/Ch13Ex1/src/Ch13Ex1.java
@@ -11,6 +11,13 @@ import acm.program.*;
 
 public class Ch13Ex1 extends ConsoleProgram {
 	public void run() {
+		SimpleStringMap states = new SimpleStringMap();
+		
+		states.put("TX", "Texas");
+		states.put("TN", "Tennessee");
+		states.put("GA", "Georgia");
+		
+		println(states.get("GA"));
 		
 	}
 }
--- Ch13/Ch13Ex1/src/SimpleStringMap.java
+++ Ch13/Ch13Ex1/src/SimpleStringMap.java
@@ -1,12 +1,42 @@
 
 public class SimpleStringMap {
 	public SimpleStringMap() {
-		int[] keyArray = int[]
-	}
-}
-
-class HashEntry {
-	public HashEntry(String key, String value) {
+		keyArray = new String[INITIAL_CAPACITY];
+		valueArray = new String[INITIAL_CAPACITY];
 		
 	}
+	
+	public void put(String key, String value) {
+		if (indexNumber >= keyArray.length) {
+			keyArray = doubleArrayCapacity(keyArray);
+			valueArray = doubleArrayCapacity(valueArray);
+		}
+		keyArray[indexNumber] = key;
+		valueArray[indexNumber] = value;
+		indexNumber++;
+	}
+	
+	public String get(String key) {
+		for (int i = 0; i < keyArray.length; i++) {
+			if (keyArray[i].equals(key)) {
+				return valueArray[i];
+			}
+		}
+		return null;
+	}
+	
+	private String[] doubleArrayCapacity(String[] oldArray) {
+		String[] newArray = new String[2 * oldArray.length];
+		for (int i = 0; i < oldArray.length; i++) {
+			newArray[i] = oldArray[i];
+		}
+		return newArray;
+	}
+	
+	private int indexNumber = 0;
+	private String[] keyArray;
+	private String[] valueArray;
+	private static final int INITIAL_CAPACITY = 2;
 }
+
+
